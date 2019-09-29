a8094b82f3bad70bc653fffa6e9b8f39853348ac
Ikeogu <ikeogu322@gmail.com>
2019-09-29 22:11:39 +0100

    update

--- resources/views/main/trustees.blade.php
+++ resources/views/main/trustees.blade.php
@@ -5,7 +5,7 @@
             <div class="row">
                 <div class="col-12 col-sm-6 col-md-4 mx-auto pt-5">
                 <div class="container">
-                    <img src="/img/trutee.jpg" class="img-fluid" alt="">
+                    <img src="/img/trutee.png" class="img-fluid" alt="">
                 </div>
                 </div>
                 <div class="col-12 col-sm-6 col-md-8 mx-auto pt-4">
